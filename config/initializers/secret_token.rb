056cecb9baa2a6cf76bfcc7236ad8352f1172365
scribblas <scribblas@gmail.com>
2012-08-24 22:49:23 +0300

    Nauja demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9f2c0866c674289640aba6a71425f1c11788cc489c7524573d687883011e50d3f66f03f9ace5194dc284ec2a0a7669db1d066e2f834c0a8a0046b6f03efbdd5d'
